fa624b735718dbad4a40ca894bbc6573d6b3cbc4
Denisio Rodrigues <denisio@ymail.com>
2023-04-09 01:42:14 -0300

    Buscando transações

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,7 +10,8 @@ interface Transaction {
 }
 
 interface TransactionContextType {
-  transactions: Transaction[]
+  transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TrsnsactionProviderProps {
@@ -22,26 +23,27 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: TrsnsactionProviderProps) {
   const[transactions, setTransactions] = useState<Transaction[]>([])
 
-  //Usando com async/await
-  async function loadTransations() {
-    const response = await fetch('http://localhost:3000/transactions');
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3000/transactions')
+
+    if(query) {
+      url.searchParams.append('q', query)
+    }
+    
+    const response = await fetch(url);
     const data = await response.json()
 
     setTransactions(data)
   }
 
-  // Outra forma de fazer
-  // async function loadTransationsNoAsync() {
-  //   fetch('http://localhost:3000/transactions')
-  //   .then((response) => { return response.json()})
-  //   .then((data) => {console.log(data)})
-  // }
-
   useEffect(() => {
-    loadTransations()
+    fetchTransactions()
   },[])
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ 
+      transactions,
+      fetchTransactions,
+    }}>
       {children}
     </TransactionsContext.Provider>
   )
